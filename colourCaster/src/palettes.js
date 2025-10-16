f297c9250a61d3367f5104c8e41a840bbd603f3c
Nimira43 <n.rabone72@aol.com>
2025-10-16 06:37:03 +0100

    getPalette function

--- colourCaster/src/palettes.js
+++ colourCaster/src/palettes.js
@@ -3,3 +3,8 @@ const palettes = {
   scroll: ['#F5DEB3', '#D2B48C', '#EEE8AA', '#DEB887'],
   rave: ['#FF00FF', '#00FF00', '#FFFF00', '#00FFFF']
 }
+
+export function getPalette(theme) {
+  const pool = palettes[theme] || palettes['glyphwave']
+  return pool[Math.floor(Math.random() * pool.length)]
+}

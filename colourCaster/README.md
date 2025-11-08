a8ce98ac9afd3082848495e7f39f68e53fcd6d42
Nimira43 <n.rabone72@aol.com>
2025-11-08 06:25:40 +0000

    Moved the package code out of project for time being.

--- colourCaster/README.md
+++
@@ -1,9 +0,0 @@
-# 🎨 ColourCaster
-
-> Cast random or themed colours with visible terminal output.
-
-## ✨ Install
-
-```bash
-npm i colourcaster
-
--- colourCaster/src/utils/formatter.js
+++
@@ -1,6 +0,0 @@
-import chalk from 'chalk'
-
-export function formatColourLog(hex, theme) {
-  const styled = chalk.hex(hex)(`[${theme.toUpperCase()}] ${hex}`)
-  console.log(styled)
-}

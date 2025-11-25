69bbdb293d4fc0d57cb3bc8ff653e38dfe844ab8
Nimira43 <n.rabone72@aol.com>
2025-11-25 01:08:32 +0000

    Updated formatter.ts

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,6 +1,9 @@
 import chalk from 'chalk'
 
-export function formatColourLog(hex, theme) {
+export function formatColourLog(
+  hex: string, 
+  theme: string
+): void {
   const styled = chalk.hex(hex)(`[${theme.toUpperCase()}] ${hex}`)
   console.log(styled)
 }

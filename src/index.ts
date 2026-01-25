3889d1a2f9fa493655431d200cba8c8a6c4a29b1
Nimira43 <n.rabone72@aol.com>
2026-01-25 14:39:16 +0000

    Rebooted project.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,2 @@
-import { getPalette, getRandomHex } from './palettes.js'
-import { formatColourLog } from './utils/formatter.js'
-
-export function castColour(
-  theme: string = 'random', 
-  log: boolean = true
-): string {
-  const colour = theme === 'random'
-    ? getRandomHex()
-    : getPalette(theme)
-
-  if (log) formatColourLog(colour, theme)
-  return colour
-}
+export { eveningGreet } from './eveningGreet'
+export { morningGreet } from './morningGreet'
